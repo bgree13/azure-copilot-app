ddbe5ecfe09c429355ca285b71ac16e6ad41bdeb
Breon.Green@globalpay.com <Breon.Green@globalpay.com>
2025-04-30 13:05:42 -0400

    change port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
